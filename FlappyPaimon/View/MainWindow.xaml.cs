af6d401be3857542deec5ea1d387639b7c7abd60
CSharperMantle <mantlejonse@gmail.com>
2021-04-24 10:08:53 +0800

    Add basic project structure

---
+++ FlappyPaimon/View/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Diagnostics;
+
+
+namespace FlappyPaimon.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchGitHubRepo(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/CSharperMantle/FlappyPaimon");
+        }
+    }
+}
